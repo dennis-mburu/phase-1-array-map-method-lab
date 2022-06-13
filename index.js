dd14b2ebc4402d45f6afa43e032205aafa431662
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2022-06-13 15:09:03 +0300

    commit tree be281b16ab3c7df193981701b5e952a375b47158

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const tutorials = [
+let tutorials = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
   'implementing Blockchain Web API',
@@ -11,6 +11,153 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
+
+
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((list) => {
+    const title = list.split(" ");
+    const capitalizedTitle = title.map(
+      (title) => title.charAt(0).toUpperCase() + title.slice(1));
+    const result = capitalizedTitle.join(' ');
+    return result;
+    });
+};
+
+
+console.log(titleCased(tutorials))
+
+
+
+
+// const titleCased = () => {
+//   return tutorials.map((list) => {
+//     const title = list.split(" ");
+//     const capitalizedTitle = title.map(
+//       (title) => title.charAt(0).toUpperCase() + title.slice(1));
+//     const result = capitalizedTitle.join(' ');
+//     return result;
+
+
+// const titleCased = () => {
+//   let tutorials2 = tutorials.map(function (elem){
+//     let sentence = elem.toLocaleLowerCase().split(" ");
+//     for (let i=0; i<sentence.length; i++ ){
+//         sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
+//     }
+//     sentence.join(" ");
+//     return sentence
+//   })
+//   tutorials.splice(0, tutorials.length)
+//   console.log(tutorials2)
+//   for (elem of tutorials2){
+//     tutorials.push(elem)
+//     tutorials.join(" ")
+
+//   }
+//   return tutorials
+// }
+
+// console.log(titleCased(tutorials))
+
+
+
+// const titleCased = () => {
+//   return tutorials.map((list) => {
+//     const title = list.split(" ");
+//     const capitalizedTitle = title.map(
+//       (title) => title.charAt(0).toUpperCase() + title.slice(1));
+//     const result = capitalizedTitle.join(' ');
+//     return result;
+
+    
+//   });
+// };
+
+// console.log(titleCased(tutorials))
+
+
+
+
+// const titleCased = () => {
+//   tutorials.map(function (elem){
+//     let sentence = elem.toLowerCase().split(" "); //sentence is is now an array with the individual words of each item in an array as its elements
+//     for (let i=0; i<sentence.length; i++ ){
+//       let word
+//       sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
+//       word = sentence[i];
+//       console.log(word)
+//       for (let i=0; i<sentence.length; i++ ){
+//         tutorials[i] = word
+//         return tutorials
+//       }
+//     }
+//     tutorials.join(" ");
+//     return tutorials
+//   })
+//   return tutorials
+// }
+
+// console.log(titleCased(tutorials))
+
+
+// const titleCased = () => {
+//   tutorials.map(function (elem){
+//     sentence = elem.toLocaleLowerCase().split(" ");
+//     for (let i=0; i<sentence.length; i++ ){
+//         tutorials[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
+//     }
+//     tutorials.join(" ");
+//     return tutorials
+//   })
+//   return tutorials
+// }
+
+// console.log(titleCased(tutorials))
+
+
+
+// const titleCased = () => {
+//   tutorials = tutorials.map(function (elem){
+//     sentence = elem.toLocaleLowerCase().split(" ");
+//     for (let i=0; i<sentence.length; i++ ){
+//         sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
+//     }
+//     sentence.join(" ");
+//     return sentence
+//   })
+//   return tutorials
+// }
+
+// console.log(titleCased(tutorials))
+
+
+
+
+
+
+// const titleCased = () => {
+//   let tut=[...tutorials]
+//   // console.log(tut)
+//   function titleCase(string) {
+//     var sentence = string.toLowerCase().split(" ");
+//     for(var i = 0; i< sentence.length; i++){
+//        sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
+//     }
+//   sentence.join(" ");
+//   return sentence
+//   }  
+//   tut.map(titleCase())
+//   console.log(tut)
+
+//   // return tutorials
+// }
+
+
+
+// const titleCased = () => {
+//   return tutorials
+// }
+
+// titleCased()
+
